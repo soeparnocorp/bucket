03073024f8f22355f7ab2a4a7ad6f6cd97cc60b5
SOEPARNO ENTERPRISE <soeparno.corp@outlook.com>
2026-01-01 18:01:36 +0700

    Fix targetBucket email address in R2Explorer config

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { R2Explorer } from 'r2-explorer';
 
-export default R2Explorer({ readonly: false, emailRouting, basicAuth: [{
+export default R2Explorer({ readonly: false, emailRouting: [{
     targetBucket: 'readtalk@outlook.co.id'
     username: 'readtalk',
     password: '@1500Tahun'
   },{
-    targetBucket: 'soepwrno.corp@outlook.com
+    targetBucket: 'soeparno.corp@outlook.com
     username: 'soeparnocorp',
     password: '12345678'
   }]
