9f0822a7fbd7bea67d6fb853c3601f69288a65a6
SOEPARNO ENTERPRISE <soeparno.corp@outlook.com>
2026-01-01 18:00:32 +0700

    Add email routing and target bucket to R2Explorer

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { R2Explorer } from 'r2-explorer';
 
-export default R2Explorer({ readonly: false, basicAuth: [{
+export default R2Explorer({ readonly: false, emailRouting, basicAuth: [{
+    targetBucket: 'readtalk@outlook.co.id'
     username: 'readtalk',
     password: '@1500Tahun'
   },{
+    targetBucket: 'soepwrno.corp@outlook.com
     username: 'soeparnocorp',
     password: '12345678'
   }]
