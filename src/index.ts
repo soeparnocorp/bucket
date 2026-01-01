4c21285bbdd045f05793a18a3f5d96185de29422
SOEPARNO ENTERPRISE <soeparno.corp@outlook.com>
2026-01-01 18:09:43 +0700

    Simplify emailRouting configuration in index.ts
    Refactor emailRouting configuration to a single targetBucket.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,7 @@
 import { R2Explorer } from 'r2-explorer';
 
-export default R2Explorer({ readonly: false, emailRouting: [{
-    targetBucket: 'readtalk@outlook.co.id'
-    username: 'readtalk',
-    password: '@1500Tahun'
-  },{
-    targetBucket: 'soeparno.corp@outlook.com
-    username: 'soeparnocorp',
-    password: '12345678'
-  }]
+export default R2Explorer({
+  readonly: false, emailRouting: {
+    targetBucket: 'bucket'
+  }
 });
