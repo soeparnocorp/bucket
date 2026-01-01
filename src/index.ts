1960fdbf9f32aecc6eaba77a8ea01a1498e8a9a6
SOEPARNO ENTERPRISE <soeparno.corp@outlook.com>
2026-01-01 18:19:19 +0700

    basicAuth to R2Explorer configuration

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { R2Explorer } from 'r2-explorer';
 
-export default R2Explorer({
-  readonly: false, emailRouting: {
-    targetBucket: 'bucket'
+export default R2Explorer({ readonly: false, basicAuth: {
+    username: 'readtalk',
+    password: '@1500Tahun'
   }
 });
