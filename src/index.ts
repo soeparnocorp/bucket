4234c307bbb9df0b4cd5dc4c6fa2dc92df72d3c2
SOEPARNO ENTERPRISE <soeparno.corp@outlook.com>
2026-09-26 22:55:00 +0700

    Add R2Explorer with basic authentication setup

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { R2Explorer } from 'r2-explorer';
+
+export default R2Explorer({ readonly: false, basicAuth: [{
+    username: 'admin',
+    password: 'admin'
+  },{
+    username: 'username',
+    password: '1235678'
+  }]
+});
